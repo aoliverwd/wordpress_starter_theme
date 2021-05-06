63391352b3946d233bded56f668bf380462ca097
Alex Oliver <alexoliverwd@gmail.com>
2021-05-06 23:35:07 +0100

    Set with of Gutenberg block editor

--- includes/wp-actions.php
+++ includes/wp-actions.php
@@ -11,10 +11,23 @@
 
     add_action('after_setup_theme', 'Theme\Actions\enable_post_thumbnails');
 
-    //show menus under appearance section on WP admin
+    /** show menus under appearance section on WP admin */
     function register_my_menu() {
         register_nav_menu('header-menu',__( 'Header Menu' ));
     }
 
     add_action('init', 'Theme\Actions\register_my_menu');
+
+
+    /** Set with of Gutenberg block editor */
+    function gutenberg_admin_styles(){
+        echo '<style>
+            .wp-block {
+                max-width: calc(100% - 80px);
+
+            }
+        </style>';
+    }
+
+    add_action('admin_head', 'Theme\Actions\gutenberg_admin_styles');
 ?>
